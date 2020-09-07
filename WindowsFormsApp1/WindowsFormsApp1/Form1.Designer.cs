285f9fd28849ea3ce2772644157e85e2cc463490
Elouuaan <elouanreymond@bluewin.ch>
2020-09-07 14:14:19 +0200

    création lable

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -28,20 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblSaise = new System.Windows.Forms.Label();
+            this.lblCopie = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblSaise
+            // 
+            this.lblSaise.AutoSize = true;
+            this.lblSaise.Location = new System.Drawing.Point(10, 27);
+            this.lblSaise.Name = "lblSaise";
+            this.lblSaise.Size = new System.Drawing.Size(41, 13);
+            this.lblSaise.TabIndex = 0;
+            this.lblSaise.Text = "Saisie :";
+            // 
+            // lblCopie
+            // 
+            this.lblCopie.AutoSize = true;
+            this.lblCopie.Location = new System.Drawing.Point(10, 71);
+            this.lblCopie.Name = "lblCopie";
+            this.lblCopie.Size = new System.Drawing.Size(40, 13);
+            this.lblCopie.TabIndex = 1;
+            this.lblCopie.Text = "Copie :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(104, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(285, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(302, 127);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(558, 154);
+            this.ClientSize = new System.Drawing.Size(401, 162);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblCopie);
+            this.Controls.Add(this.lblSaise);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblSaise;
+        private System.Windows.Forms.Label lblCopie;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
