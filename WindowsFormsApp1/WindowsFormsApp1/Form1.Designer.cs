d35db4382120bd5dce06d9fff4ece45443867c2f
Elouuaan <elouanreymond@bluewin.ch>
2020-09-14 12:55:58 +0200

    fromat cellule

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -30,10 +30,10 @@
         {
             this.lblSaise = new System.Windows.Forms.Label();
             this.lblCopie = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.tbxSaisie = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.lblResuét = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblSaise
@@ -54,48 +54,49 @@
             this.lblCopie.TabIndex = 1;
             this.lblCopie.Text = "Copie :";
             // 
-            // textBox1
+            // tbxSaisie
             // 
-            this.textBox1.Location = new System.Drawing.Point(104, 24);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(285, 20);
-            this.textBox1.TabIndex = 3;
+            this.tbxSaisie.Location = new System.Drawing.Point(104, 24);
+            this.tbxSaisie.Name = "tbxSaisie";
+            this.tbxSaisie.Size = new System.Drawing.Size(285, 20);
+            this.tbxSaisie.TabIndex = 3;
             // 
-            // button1
+            // btnDelete
             // 
-            this.button1.Location = new System.Drawing.Point(13, 127);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(87, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnDelete.Location = new System.Drawing.Point(13, 127);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(87, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Effacer";
+            this.btnDelete.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnQuit
             // 
-            this.button2.Location = new System.Drawing.Point(302, 127);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(87, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnQuit.Location = new System.Drawing.Point(302, 127);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(87, 23);
+            this.btnQuit.TabIndex = 5;
+            this.btnQuit.Text = "Quitter";
+            this.btnQuit.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // lblResuét
             // 
-            this.label1.Location = new System.Drawing.Point(104, 70);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(285, 23);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "label1";
+            this.lblResuét.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblResuét.Location = new System.Drawing.Point(104, 70);
+            this.lblResuét.Name = "lblResuét";
+            this.lblResuét.Size = new System.Drawing.Size(285, 23);
+            this.lblResuét.TabIndex = 6;
+            this.lblResuét.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(401, 162);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblResuét);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.tbxSaisie);
             this.Controls.Add(this.lblCopie);
             this.Controls.Add(this.lblSaise);
             this.Name = "Form1";
@@ -109,10 +110,10 @@
 
         private System.Windows.Forms.Label lblSaise;
         private System.Windows.Forms.Label lblCopie;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxSaisie;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Label lblResuét;
     }
 }
 
